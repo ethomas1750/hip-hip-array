8425306faee6a85a818d6a8ef053c55c0fd7d5aa
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-05 21:06:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+/* This assignment will give you some practice with creating arrays and using their methods. */
 
+// ANY time you're asked to print something from the  array, do it with a loop, printing each item you're asked to print INDIVIDUALLY.
+// 
+// 
+//
+// Challenge 1
+//
+// Loop through the following array, console.logging out each value.
